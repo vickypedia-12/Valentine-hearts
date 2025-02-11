8f74841aaa6256b8f05dac38ed2100a8f70d23fe
vickypedia-12 <vikasmourya54321@gmail.com>
2025-02-12 00:38:35 +0530

    Fix database migration issues in deployment

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+
+python manage.py makemigrations
+python manage.py migrate --noinput
+
 # Run migrations
-python manage.py migrate
+python manage.py createsuperuser --noinput || true
